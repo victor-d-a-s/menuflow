7ac65c6f9473074f6bbe0d93aadb464641bc1da4
victor-d-a-s <victorsouzaagv@gmail.com>
2026-03-23 19:17:19 -0300

    chore: adiciona configuração Next.js

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss"
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [require("tailwindcss-animate")],
+}
+export default config
