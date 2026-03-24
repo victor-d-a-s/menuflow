d60e88e38406209e924081f3b6222b4b989d5d4d
victor-d-a-s <victorsouzaagv@gmail.com>
2026-03-23 22:14:26 -0300

    chore: configura runtime edge em todas as rotas

--- app/(super-admin)/restaurantes/novo/page.tsx
+++ app/(super-admin)/restaurantes/novo/page.tsx
@@ -1,3 +1,4 @@
+export const runtime = "edge";
 import { getServerUser } from '@/lib/supabase/server'
 import { redirect } from 'next/navigation'
 import { CreateRestaurantForm } from './components/create-restaurant-form'
