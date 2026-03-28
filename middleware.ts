ad93b8ceeefa77545d3099a5e5ef70ad420b16d5
victor-d-a-s <victorsouzaagv@gmail.com>
2026-03-27 21:26:27 -0300

    fix: tipo no middleware

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export async function middleware(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name) { return request.cookies.get(name)?.value },
+        get(name: string) { return request.cookies.get(name)?.value },
         set(name, value, options) {
           request.cookies.set({ name, value, ...options })
           response = NextResponse.next({ request: { headers: request.headers } })
